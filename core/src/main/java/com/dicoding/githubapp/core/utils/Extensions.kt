afdd379e2e93b1645b3120d71fcab36d6fbd40c5
fachridantm <fachridantm@gmail.com>
2023-03-27 22:57:57 +0700

    remove parcelable arraylist

--- core/src/main/java/com/dicoding/githubapp/core/utils/Extensions.kt
+++ core/src/main/java/com/dicoding/githubapp/core/utils/Extensions.kt
@@ -57,10 +57,3 @@ inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
     SDK_INT >= 33 -> @Suppress("DEPRECATION") getParcelableExtra(key)
     else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
 }
-
-inline fun <reified T : Parcelable> Intent.parcelableArrayList(key: String): ArrayList<T>? {
-    return when {
-        SDK_INT >= 33 -> getParcelableArrayListExtra(key, T::class.java)
-        else -> @Suppress("DEPRECATION") getParcelableArrayListExtra(key)
-    }
-}
