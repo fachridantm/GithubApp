4be202477b82b4dae29e0b72d1f2c799543d0a65
fachridantm <fachridantm@gmail.com>
2023-03-28 22:27:41 +0700

    add certificate pinner

--- core/src/main/java/com/dicoding/githubapp/core/di/NetworkModule.kt
+++ core/src/main/java/com/dicoding/githubapp/core/di/NetworkModule.kt
@@ -1,11 +1,12 @@
 package com.dicoding.githubapp.core.di
 
-import com.dicoding.githubapp.core.BuildConfig
+import com.dicoding.githubapp.core.BuildConfig.*
 import com.dicoding.githubapp.core.data.source.remote.network.MainApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +18,14 @@ import java.util.concurrent.TimeUnit
 class NetworkModule {
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(HOSTNAME, "sha256/1UPHAdcUbUoOcd5rDTD/0oMSnngCU6YzXzpByO4CCp4=")
+            .add(HOSTNAME, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(
                 HttpLoggingInterceptor().setLevel(
-                    if (BuildConfig.DEBUG) {
+                    if (DEBUG) {
                         HttpLoggingInterceptor.Level.BODY
                     } else {
                         HttpLoggingInterceptor.Level.NONE
@@ -29,20 +34,21 @@ class NetworkModule {
             )
             .addInterceptor { chain ->
                 chain.request().newBuilder()
-                    .addHeader("Authorization","token ${BuildConfig.GITHUB_TOKEN}")
+                    .addHeader("Authorization","token $GITHUB_TOKEN")
                     .build()
                     .let(chain::proceed)
             }
             .connectTimeout(60, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(60, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
     @Provides
     fun provideMainApiService(client: OkHttpClient): MainApiService {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
+            .baseUrl(BASE_URL)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
